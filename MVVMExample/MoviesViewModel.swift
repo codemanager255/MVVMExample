e1d9af457b6f48196dd8875ce6d57763795821f9
codemanger255 <codemanager255@gmail.com>
2019-09-03 17:17:15 +0530

    added new viewModel

---
+++ MVVMExample/MoviesViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MoviesViewModel.swift
+//  MVVMExample
+//
+//  Created by MAC on 03/09/19.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import Foundation
+
+
+class MoviesViewModel {
+    
+}
