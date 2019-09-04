d98710202f5eca07d61d82e17ffb6551f2f260e2
codemanger255 <codemanager255@gmail.com>
2019-09-05 00:14:00 +0530

    added network manager class

---
+++ MVVMExample/NetworkManager.swift
@@ -0,0 +1,15 @@
+
+//
+//  NetworkManager.swift
+//  MVVMExample
+//
+//  Created by MAC on 05/09/19.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import Foundation
+
+
+class NetworkManager {
+    
+}
